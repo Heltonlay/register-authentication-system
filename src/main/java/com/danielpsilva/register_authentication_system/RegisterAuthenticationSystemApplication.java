63d8c61600a73eeb4223a30ec2e1bbe07a08251d
Heltonlay <heltonlay15@gmail.com>
2024-10-28 08:58:25 -0300

    Program created

---
+++ src/main/java/com/danielpsilva/register_authentication_system/RegisterAuthenticationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.danielpsilva.register_authentication_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAuthenticationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterAuthenticationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielpsilva/cadastro_autenticacao/CadastroAutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielpsilva.cadastro_autenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAutenticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
